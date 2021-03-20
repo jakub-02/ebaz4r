98a07bbe3334a208b0be6d056e6b514cc6a676aa
jakub_02 <baransky@spse-po.sk>
2021-03-20 16:03:11 +0100

    funkcny register(dufam)

---
+++ app/src/main/java/com/example/eb4zar/model/UserDetail.java
@@ -0,0 +1,17 @@
+package com.example.eb4zar.model;
+
+public class UserDetail {
+    private String meno;
+
+    public UserDetail(String meno){
+        this.meno = meno;
+    }
+
+    public String getMeno() {
+        return meno;
+    }
+
+    public void setMeno(String meno) {
+        this.meno = meno;
+    }
+}
