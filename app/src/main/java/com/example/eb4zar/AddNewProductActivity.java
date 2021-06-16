56fec96b68b5418d82a6f1ad234dcc9cdf72db91
jakub_02 <baransky@spse-po.sk>
2021-06-16 17:25:40 +0200

    vymazal som toolbar z pridavania inzeratov ;)

--- app/src/main/java/com/example/eb4zar/AddNewProductActivity.java
+++ app/src/main/java/com/example/eb4zar/AddNewProductActivity.java
@@ -1,12 +1,14 @@
 package com.example.eb4zar;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +17,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
